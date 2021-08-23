acafb75ece4a059855c2ed2f9c20c6156d10db72
hardik5627 <88597096+hardik5627@users.noreply.github.com>
2021-08-24 00:43:19 +0530

    Update logic.py

--- SimpleSimulator/logic.py
+++ SimpleSimulator/logic.py
@@ -9,12 +9,89 @@ class LU:
 	def sub(params):
 		"subtracts two integers"
 		return {
-			'main': params[0] - params[1]
+			'main': params[0] - params[1],
+			'flags': f"{int(params[0]-params[1] > 255)}001"
+		}
+	
+	def movi(params):
+		"Move Immediate"
+		return {
+			'main': params[0] + params[1],
+			'flags': f"{int(params[0] + params[1] > 255)}010"
 		}
 
+	def movr(params):
+		"Move Register"
+		return {
+			'main': params[0] - params[1],
+			'flags': f"{int(params[0] + params[1] > 255)}011"
+		}
+	
+	def ld(params):
+		"Loads data"
+		return {
+			'main': params[0] + params[1],
+			'flags': f"{int(params[0] + params[1] > 255)}100"
+		}
+
+	def st(params):
+		"Store"
+		return {
+			'main': params[0] - params[1],
+			'flags': f"{int(params[0] + params[1] > 255)}101"
+		}
+	def mul(params):
+		"multiplies two integers"
+		return {
+			'main': params[0] * params[1],
+			'flags': f"{int(params[0] * params[1])}110"
+		}
+
+	def div(params):
+		"divides two integers"
+		return {
+			'main': params[0]/params[1],
+			'flags': f"{int(params[0] / params[1] > 255)}111"
+		}
+	def rs(params):
+		"RIGHT SHIFT"
+		return {
+			'main': params[0] + params[1],
+			'flags': f"{int(params[0] + params[1] > 255)}1000"
+		}
+
+	def ls(params):
+		"LEFT SHIFT"
+		return {
+			'main': params[0] - params[1],
+			'flags': f"{int(params[0] + params[1] > 255)}1001"
+		}
+	def hlt(params):
+		"stops running code"
+		return {}
+	
+
 	switcher = {
-		0: add,
-		1: sub
+		0b00000: add,
+		0b00001: sub,
+		0b00010: movi,
+		0b00011: movr,
+		0b00100: ld,
+		0b00101: st,
+		0b00110: mul,
+		0b00111: div,
+		0b01000: rs,
+		0b01001: ls,
+		0b01010: xor,
+		0b01011: or,
+		0b01100: and,
+		0b01101: not,
+		0b01110: cmp,
+		0b01111: jmp,
+		0b10000: jlt,
+		0b10001: jgt,
+		0b10010: je,
+		0b10011: hlt,
 	}
 
 	@classmethod
